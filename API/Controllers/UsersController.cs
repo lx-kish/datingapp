83963758e085a7b854e2d709d2fc00539d4d1f14
lx-kish <urban.ktulhu@gmail.com>
2025-04-18 20:32:30 +1200

    feat(core): convert users routes to asynchronous
    - convert GetUsers method to asynchronous and change returning value to Task
    
    - convert GetUser method to asynchronous and change returning value to Task
    
    - update .gitignore

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API;
 
@@ -9,17 +10,17 @@ namespace API;
 public class UsersController(DataContext context) : ControllerBase
 {
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = context.Users.ToList();
+        var users = await context.Users.ToListAsync();
 
         return users;
     }
 
     [HttpGet("{id:int}")] // /api/users/{id}
-    public ActionResult<AppUser> GetUser(int id)
+    public async Task<ActionResult<AppUser>> GetUser(int id)
     {
-        var user = context.Users.Find(id);
+        var user = await context.Users.FindAsync(id);
 
         if (user == null) return NotFound();
 

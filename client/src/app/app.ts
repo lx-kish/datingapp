7b2d3b3138a68922978a8bde7bd28fa79eb88e4e
lx-kish <urban.ktulhu@gmail.com>
2025-04-22 21:18:52 +1200

    feat(core): add http client
    - add HTTP provider at app.config.ts
    
    - add OnInit method making HTTP request to app.ts class

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  private http = inject(HttpClient);
   protected title = 'DatingApp';
+
+  ngOnInit(): void {
+    const address = `https://localhost:5001/api/members`
+    this.http.get(address).subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log(`http request ${address} completed`)
+    })
+  }
 }

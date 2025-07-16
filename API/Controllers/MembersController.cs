5a6dd00182504d042f070231ad421fa3faf59102
lx-kish <urban.ktulhu@gmail.com>
2025-07-16 20:41:40 +1200

    feat(core): add claims principal extensions
    - add ClaimsPrincipalExtensions.cs static class with static method GetMemberId to be able to extract user Id from the JWT claim userId
    
    - add GetMemberForUpdate method to the IMemberRepository interface
    
    - add implementation of the GetMemberForUpdate method in the MemberRepository class which returns member and user entities based on passed string Id argument
    
    - adjust UpdateMember method of the MemberController to consume GetMemberForUpdate method of MemberRepository class to be able to update member details and user details in a single run

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,6 +1,6 @@
-using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -35,11 +35,9 @@ public class MembersController(IMemberRepository memberRepository) : BaseApiCont
     [HttpPut]
     public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
     {
-        var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var memberId = User.GetMemberId();
 
-        if (memberId == null) return BadRequest("No id found in the token");
-
-        var member = await memberRepository.GetMemberByIdAsync(memberId);
+        var member = await memberRepository.GetMemberForUpdate(memberId);
 
         if (member == null) return BadRequest("Could not get member");
 
@@ -47,6 +45,8 @@ public class MembersController(IMemberRepository memberRepository) : BaseApiCont
         member.Description = memberUpdateDto.Description ?? member.Description;
         member.City = memberUpdateDto.City ?? member.City;
         member.Country = memberUpdateDto.Country ?? member.Country;
+
+        member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
 
         memberRepository.Update(member);
 

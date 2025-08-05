c556fad250160c1ee46358dea7a77cfda638711f
lx-kish <urban.ktulhu@gmail.com>
2025-08-05 17:50:55 +1200

    feat(core): add UI for deleting photo
    - add deletePhoto method sending DELETE request to the API at the MemberService class
    
    - add delete-button angular component
    
    - add delete button functionality at delete-button.ts class
    
    - add layout to the delete-button.html template
    
    - add delete-button component to the member-photos.html layout
    
    - add conditional rendering of star-button and delete-button components depending on the current user at member-photos.html template
    
    - add import of DeleteButton component and change accountService field access modifier to be able to use it in the member-photos.html template at the MemberPhotos component
    
    - adjust class in the star-button.html template to make it less specific and be able to pass class to the component as properties

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -6,16 +6,17 @@ import { ImageUpload } from "../../../shared/image-upload/image-upload";
 import { AccountService } from '../../../core/services/account-service';
 import { IUser } from '../../../types/user';
 import { StarButton } from "../../../shared/star-button/star-button";
+import { DeleteButton } from '../../../shared/delete-button/delete-button';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [ImageUpload, StarButton],
+  imports: [ImageUpload, StarButton, DeleteButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
 export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService);
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
   protected photos = signal<IPhoto[]>([]);
   protected loading = signal(false);
@@ -59,6 +60,14 @@ export class MemberPhotos implements OnInit {
           }) as IMember)
         }
 
+      }
+    })
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: () => {
+        this.photos.update(photos => photos.filter(x => x.id !== photoId));
       }
     })
   }
